80fb26bfed6902517d3b6e773fc87a45f533857a
alexandr baranoc <sabbarik5@gmail.com>
2024-11-08 03:24:57 +0300

    добавлен фронт

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,6 +15,10 @@ func main(){
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/snippet/show", showSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
